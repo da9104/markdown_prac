9c243c0287dd862b45b19b00debb913150bae9f1
DAMI <damikang@DAMIs-MacBook-Pro.local>
2023-05-24 23:28:21 +0100

    practice markdown.

--- README.md
+++ README.md
@@ -3,5 +3,24 @@
 ### heading 3 *italic*
 
 
-Still practicing it.
+Still practicing it. \
 have a good day!
+
+```
+this paragraph will be code
+
+Javascript:
+
+var objName = {
+    name: "dami",
+    address: "GB"
+}
+
+Python function:
+
+def say_hello(you):
+    return "Hello " +  you
+
+print(say_hello("you"))
+
+```
--- py.py
+++ py.py
@@ -1,1 +1,23 @@
-print("hello world")
+# print("hello world")
+
+# a = "heelo multi line" \
+#     "this is second line"
+
+# print(a)
+
+# a = "my favorite food is " 
+# b = "mashed potatoes" 
+
+# print(a+b)
+
+
+def say_hello():
+    return "Hello there!"
+
+# say_hello()
+
+# With parameters
+def say_hello(you):
+    return "Hello " +  you
+
+print(say_hello("you"))
